8b9e4677c1958098cc69374de1007a157459393b
HahieuPAC <hahieupac@gmail.com>
2023-07-29 16:08:14 +0700

    old ubuntu

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,11 +1,23 @@
-import { Controller } from "@nestjs/common";
+import { Controller, Post } from "@nestjs/common";
 import { AuthService } from "./auth.service";
-@Controller()
+@Controller('auth')
 export class AuthController {
     // auth service is automatically created when initializing the controller
     constructor(private authService: AuthService) {
 
     }
     // some request from client
+    @Post('register') //register a new user
+    register() {
+        // Now controller call 'service'
+        return this.authService.register
+    }
+
+
+    //POST: ..../auth/login
+    @Post('login')
+    login() {
+        return this.authService.login();
+    }
 }
 // export = "make public"
--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,3 +1,17 @@
 import { Injectable } from "@nestjs/common";
 @Injectable({}) // This us "Dependency"
-export class AuthService {}
+export class AuthService {
+    register () {
+        return {
+            message: 'Register an user'
+        }
+        
+    }
+    
+    login() {
+        return {
+            message: 'This is Login'
+        }
+        
+    }
+}
